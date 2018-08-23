5803a6305efdffc350e0e6ba3fb3ee3fad781eb2
zhangweiying <zhangweiying@thejoyrun.com>
2018-08-23 11:12:48 +0800

    mysql初始化

---
+++ server/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'test',
+    USERNAME: 'root',
+    PASSWORD: '987654321',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
